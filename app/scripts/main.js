b873c9b6e614f98d41ad856ced37c4e816a2bfe2
Ian Adams <ijadams503@gmail.com>
2015-08-31 22:01:39 -0500

    client slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,12 @@
 // jshint devel:true
-
+      
 $(function() {
-    $('.banner').unslider({
-      //  Enable keyboard arrows
-      keys: true,               
-			// Enable dot nav
-      dots: true,
-      delay: 7000
-    });
+  $('.banner').unslider({
+    //  Enable keyboard arrows
+    keys: true,               
+    // Enable dot nav
+    dots: true,
+    delay: 7000
+  });
 });
-
-
 
